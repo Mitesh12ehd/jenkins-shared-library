a8de59bf388b96afe963b1cd7f6ae4bdae933180
mitesh <miteshchavda57@gmail.com>
2025-12-18 23:43:14 +0530

    Update buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,23 +1,26 @@
 #!/user/bin/env groovy
 
-echo "building the docker image..."
-// extract username and password from credential that we have added in GUI
-withCredentials([
-        usernamePassword(
-                credentialsId: 'docker-hub-repo',
-                usernameVariable: 'USER',
-                passwordVariable: 'PASS'
-        )
-]){
-    // inside this block we have access to USER and PASS variable
+def call(){
+    echo "building the docker image..."
+    // extract username and password from credential that we have added in GUI
+    withCredentials([
+            usernamePassword(
+                    credentialsId: 'docker-hub-repo',
+                    usernameVariable: 'USER',
+                    passwordVariable: 'PASS'
+            )
+    ]){
+        // inside this block we have access to USER and PASS variable
 
-    // build image
-    sh "docker build -t miteshch/demo-app:jma-2.0 ."
+        // build image
+        sh "docker build -t miteshch/demo-app:jma-2.0 ."
 
-    // login into docker, taking password from input as it become masked
-    sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        // login into docker, taking password from input as it become masked
+        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
 
-    // push image
-    sh "docker push miteshch/demo-app:jma-2.0 ."
-    sh "docker push miteshch/demo-app:jma-2.0"
+        // push image
+        sh "docker push miteshch/demo-app:jma-2.0 ."
+        sh "docker push miteshch/demo-app:jma-2.0"
+    }
 }
+
